48cbd896a0745798a07486100d51276f817a552e
Dmitriy Bondarenko <dabwork1982@gmail.com>
2021-10-20 18:31:45 +0300

    task 02 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+const imgMarkup = galleryItems
+	.map(({ preview, original, description }) => {
+		return `<li><a class="gallery__item" href="${original}">
+    <img class="gallery__image"
+        src="${preview}"
+        alt="${description}"/>
+</a></li>`;
+	})
+	.join("");
+
+galleryContainer.insertAdjacentHTML("beforeend", imgMarkup);
+
+const forOpenLightbox = {
+	captionsData: "alt",
+	captionDelay: 250,
+};
+
+const lightbox = new SimpleLightbox(".gallery__item", forOpenLightbox);
